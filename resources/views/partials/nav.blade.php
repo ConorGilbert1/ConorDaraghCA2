45b73ce3ab2e7fa79baba083c9f056bbe9c96cf2
Darragh24 <conwaydarragh03@gmail.com>
2024-05-08 15:14:10 +0100

    First commit of laravel Vite project

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,9 @@
+<nav>
+ <p>This is the Nav with random links that we can also fix later</p>
+ <ul>
+  <li><a href="/">Home</a></li>
+  <li><a href="/about">About</a></li>
+  <li><a href="/contact">Contact</a></li>
+  <!-- Add additional menu items as needed -->
+ </ul>
+</nav>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!-- resources/views/welcome.blade.php -->
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Your Website</title>
+    @vite('resources/js/app.js')
+
+</head>
+
+<body>
+    @include('partials.nav')
+
+    <div class="container">
+        <h1>Welcome to Your Website</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam nisl id metus pulvinar, vitae consectetur lectus sodales. Mauris eget justo ut enim commodo faucibus.</p>
+        <p>Nulla facilisi. Vivamus vehicula, ex at vestibulum tincidunt, justo lorem pellentesque nunc, id suscipit risus neque a turpis.</p>
+    </div>
+
+    @include('partials.footer')
+</body>
+
+</html>
