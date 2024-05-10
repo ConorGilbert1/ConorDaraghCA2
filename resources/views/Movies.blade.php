7ce392795428466cde264893da4a931ae1e397ce
Darragh24 <conwaydarragh03@gmail.com>
2024-05-10 19:19:39 +0100

    Added scss to footer

--- resources/views/Movies.blade.php
+++ resources/views/Movies.blade.php
@@ -6,12 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movies</title>
     @vite('resources/js/app.js')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
     <div class="main-container">
         @include('layouts.nav')
-        <h1>Movies</h1>
+        <div class="movies-heading">
+            Movies
+        </div>
+
         <div class="movie-container">
             @foreach($movies as $movie)
             <div class="movie">
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,26 @@
+<div class="footer-container">
 
-<footer>
- <p>This is the Footer, we can fill this out and style it whenever</p>
-
-</footer>
+ <div class="footer-link-container">
+  <p>About us</p>
+  <a href="#" class="">Link 1</a>
+  <a href="#" class="">Link 2</a>
+  <a href="#" class="">Link 3</a>
+  <a href="#" class="">Link 4</a>
+ </div>
+ <div class="footer-socials-container">
+  <p> Our Socials </p>
+  <div class="footer-socials-icons-container">
+   <a href="#" class="fa fa-facebook"></a>
+   <a href="#" class="fa fa-twitter"></a>
+   <a href="#" class="fa fa-youtube"></a>
+   <a href="#" class="fa fa-github"></a>
+  </div>
+ </div>
+ <div class="footer-contact-container">
+  <p> Contact Us </p>
+  <div class="contact-form">
+   <input>
+   <button class="contact-submit-btn"><i class="fa fa-sign-in"></i></button>
+  </div>
+ </div>
+</div>
